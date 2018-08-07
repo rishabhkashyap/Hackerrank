72eba2fbc23c01744284ccca196001474557a016
rishabh <kashyap.rishab@hotmail.com>
2018-08-07 20:51:19 +0530

    Solution for STPAR(Spoj online)

---
+++ hackerrank/src/com/hackerrank/solutions/spoj/StreetParadeQueue.java
@@ -0,0 +1,57 @@
+
+//https://www.spoj.com/problems/STPAR/
+package com.hackerrank.solutions.spoj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class StreetParadeQueue {
+    public static void main (String[] args) throws IOException {
+        BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer tokenizer=new StringTokenizer(bufferedReader.readLine());
+        Queue<Integer>mobiles=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            mobiles.add(Integer.parseInt(tokenizer.nextToken()));
+        }
+
+        boolean order=hasOrder(mobiles);
+        if(order){
+            System.out.println("yes");
+        }else {
+            System.out.println("no");
+        }
+
+
+    }
+
+    private static boolean hasOrder(Queue<Integer> mobiles) {
+        int position=1;
+        int mobileSize=mobiles.size();
+        Stack<Integer>stack = new Stack<>();
+        while (!mobiles.isEmpty() || !stack.isEmpty()){
+            if(!mobiles.isEmpty() && mobiles.peek()==position){
+                ++position;
+                mobiles.remove();
+            }else{
+                if(!stack.isEmpty() && stack.peek()==position){
+                    ++position;
+                }else{
+                    if(!mobiles.isEmpty()){
+                        stack.push(mobiles.remove());
+                    }else {
+                         break;
+                    }
+
+                }
+
+            }
+        }
+        return position==mobileSize?true:false;
+    }
+}

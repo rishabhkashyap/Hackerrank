c67d600ed3ffbf8bf894fcb741792779c2668496
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-04-18 16:18:44 +0530

    Changing dp array size

--- hackerrank/src/com/problem/solutions/leetcode/UniquePathRobot.java
+++ hackerrank/src/com/problem/solutions/leetcode/UniquePathRobot.java
@@ -10,33 +10,30 @@ public class UniquePathRobot {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int m = Integer.parseInt(bufferedReader.readLine());
         int n = Integer.parseInt(bufferedReader.readLine());
-        int[][] dp = new int[1000][1000];
+        int[][] dp = new int[m+1][n+1];
         initializeArray(dp);
         System.out.println(move(1, 1, m, n, dp));
     }
 
     private static int move(int i, int j, int m, int n, int[][] dp) {
-        if (i == m && j == n) {
+
+        if (i == m || j == n) {
             return 1;
         }
-        if (i > m || j > n) {
+        if (i > m || j >n) {
             return 0;
         }
 
-        if (dp[i + 1][j] == -1) {
-            dp[i + 1][j] = move(i + 1, j, m, n, dp);
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-
-        if (dp[i][j + 1] == -1) {
-            dp[i][j + 1] = move(i, j + 1, m, n, dp);
-        }
-
-        return dp[i + 1][j] + dp[i][j + 1];
+        dp[i][j] = move(i + 1, j, m, n, dp)+move(i, j + 1, m, n, dp);
+        return dp[i][j];
     }
 
     private static void initializeArray(int[][] dp) {
         for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp.length; j++) {
+            for (int j = 0; j < dp[0].length; j++) {
                 dp[i][j] = -1;
             }
         }

b2d1e6fb8fc0f53c87a324241364b87941cb2e46
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-07-12 06:51:22 +0530

    Adding bottom up approach to solve problem

--- hackerrank/src/com/problem/solutions/leetcode/LongestCommonSubSequence.java
+++ hackerrank/src/com/problem/solutions/leetcode/LongestCommonSubSequence.java
@@ -16,6 +16,7 @@ public class LongestCommonSubSequence {
         System.out.println(getLongestSubsequenceLength1(text1, text2));
         System.out.println(getLongestSubsequenceLength2(text1, text2));
         System.out.println(getLongestSubsequenceLength3(text1, text2));
+        System.out.println(getLongestSubsequenceLength4(text1, text2));
 
     }
 
@@ -77,5 +78,20 @@ public class LongestCommonSubSequence {
             }
         }
         return dp[0][0];
+    }
+
+    private static int getLongestSubsequenceLength4(String string1, String string2) {
+        int[][] dp = new int[string1.length() + 1][string2.length() + 1];
+        for (int i = 1; i <= string1.length(); i++) {
+            for (int j = 1; j <= string2.length(); j++) {
+                if (string1.charAt(i - 1) == string2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+
+            }
+        }
+        return dp[string1.length()][string2.length()];
     }
 }

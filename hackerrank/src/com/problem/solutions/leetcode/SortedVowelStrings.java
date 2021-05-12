9ad1a453845fec5a2d87856506183ba83f1a518d
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-05-12 18:56:32 +0530

    Count Sorted Vowel Strings

--- hackerrank/src/com/problem/solutions/leetcode/SortedVowelStrings.java
+++ hackerrank/src/com/problem/solutions/leetcode/SortedVowelStrings.java
@@ -12,25 +12,19 @@ public class SortedVowelStrings {
 
     private static int countVowelString(int n) {
         char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-        int[] dp = new int[500000];
-        Arrays.fill(dp, -1);
-        return countVowelString(vowels, n, 0, new ArrayList<Character>(), dp);
+        return countVowelString(vowels, n, 0, new ArrayList<Character>());
     }
 
-    private static int countVowelString(char[] vowels, int n, int index, ArrayList<Character> result, int[] dp) {
+    private static int countVowelString(char[] vowels, int n, int index, ArrayList<Character> result) {
         if (n == result.size()) {
             return 1;
-        }
-        if (dp[result.size()] != -1) {
-            return dp[result.size()];
         }
         int count = 0;
         for (int i = 0; i < vowels.length; i++) {
             if (result.size() == 0 || result.get(result.size() - 1) <= vowels[i]) {
                 result.add(vowels[i]);
-                count += countVowelString(vowels, n, i, result,dp);
+                count += countVowelString(vowels, n, i, result);
                 result.remove(result.size() - 1);
-                dp[result.size()] = count;
             }
         }
 

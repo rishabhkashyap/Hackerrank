a4be6d0c15376ff456ec4e88064e062034c494db
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-07-19 18:41:53 +0530

    Removing redundant code

--- hackerrank/src/com/problem/solutions/leetcode/GoodSplit1525.java
+++ hackerrank/src/com/problem/solutions/leetcode/GoodSplit1525.java
@@ -11,28 +11,24 @@ import java.util.Set;
 public class GoodSplit1525 {
     public static void main(String[] args) {
         String str = "abcd";
-        System.out.println(findGoodSplit(str));
+        System.out.println(findGoodSplits(str));
     }
 
-    private static int findGoodSplit(String str) {
+    private static int findGoodSplits(String str) {
         Map<String, Integer> map = new HashMap<>();
         //Number of unique character from 0 ... len-1
         int[] prefix = new int[str.length()];
 
         //Number of unique characters from len-1 ....0
         int[] suffix = new int[str.length()];
-        prefix[0] = 1;
         Set<Character> set = new HashSet<>();
-        set.add(str.charAt(0));
-        for (int i = 1; i < prefix.length; i++) {
+        for (int i = 0; i < prefix.length; i++) {
             set.add(str.charAt(i));
             prefix[i] = set.size();
 
         }
         set.clear();
-        suffix[str.length() - 1] = 1;
-        set.add(str.charAt(str.length() - 1));
-        for (int i = str.length() - 2; i >= 0; --i) {
+        for (int i = str.length() - 1; i >= 0; --i) {
             set.add(str.charAt(i));
             suffix[i] = set.size();
         }

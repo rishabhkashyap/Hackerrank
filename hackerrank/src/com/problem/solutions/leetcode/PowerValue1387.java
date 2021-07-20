e096febe38a4bca3a237ad9a3a8ef31e3a32183c
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-07-20 08:45:52 +0530

    1387: Power value

--- hackerrank/src/com/problem/solutions/leetcode/PowerValue1387.java
+++ hackerrank/src/com/problem/solutions/leetcode/PowerValue1387.java
@@ -1,47 +1,56 @@
 package com.problem.solutions.leetcode;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+// Problem: https://leetcode.com/problems/sort-integers-by-the-power-value/
+
 public class PowerValue1387 {
     public static void main(String[] args) {
-        int low = 12;
-        int high = 15;
-        int k = 2;
+        int low = 144;
+        int high = 163;
+        int k = 5;
         System.out.println(findPowerValue(low, high, k));
     }
 
     private static int findPowerValue(int low, int high, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        int[] dp = new int[1001];
-        Arrays.fill(dp, -1);
+        Map<Integer, Integer> dp = new HashMap<>();
         for (int i = low; i <= high; i++) {
             map.put(i, findPowerValue(i, dp));
         }
         List<Map.Entry<Integer, Integer>> list = map.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
                 .collect(Collectors.toList());
+        list.sort((e1, e2) -> compare(e1, e2));
         return list.get(k - 1).getKey();
 
     }
 
 
-    private static int findPowerValue(int num, int[] dp) {
+    private static int findPowerValue(int num, Map<Integer, Integer> dp) {
         if (num == 1) {
             return 0;
         }
-        if (dp[num] != -1) {
-            return dp[num];
+        if (dp.containsKey(num)) {
+            return dp.get(num);
         }
-        int count = 0;
+
+        int count;
         if (num % 2 == 0) {
             count = findPowerValue(num / 2, dp) + 1;
         } else {
             count = findPowerValue(num * 3 + 1, dp) + 1;
         }
+        dp.put(num, count);
         return count;
+    }
+
+    private static int compare(Map.Entry<Integer, Integer> entry1, Map.Entry<Integer, Integer> entry2) {
+        if (entry1.getValue() == entry2.getValue()) {
+            return entry1.getKey() - entry2.getKey();
+        }
+        return entry1.getValue() - entry2.getValue();
     }
 }

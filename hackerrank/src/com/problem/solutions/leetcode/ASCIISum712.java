5bb5b0028aed9e2c6c83b871b0466b73bce2236f
rishabh <rishabh.kashyap@oracle.com>
2022-02-16 17:58:46 +0530

    Leetcode 712:  Minimum ASCII Delete Sum for Two Strings

--- hackerrank/src/com/problem/solutions/leetcode/ASCIISum712.java
+++ hackerrank/src/com/problem/solutions/leetcode/ASCIISum712.java
@@ -5,8 +5,9 @@ package com.problem.solutions.leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class ASCIISum {
+public class ASCIISum712 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -16,35 +17,34 @@ public class ASCIISum {
         System.out.println(findMinASCIISum2(string1, string2));
     }
 
-    private static int findMinASCIISum1(String string1, String string2) {
-        int[][] dp = new int[string1.length() + 1][string2.length() + 1];
-        for (int i = 0; i < dp.length; ++i) {
-            for (int j = 0; j < dp[0].length; ++j) {
-                dp[i][j] = -1;
-            }
+    private static int findMinASCIISum1(String str1, String str2) {
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
         }
-        return findMinASCIISum1(string1, string1.length() - 1, string2, string2.length() - 1, dp);
+
+        return findMinASCIISum1(str1, str1.length() - 1, str2, str2.length() - 1, dp);
     }
 
-    private static int findMinASCIISum1(String string1, int i, String string2, int j, int[][] dp) {
+    private static int findMinASCIISum1(String str1, int i, String str2, int j, int[][] dp) {
         if (i < 0 && j < 0) {
             return 0;
         }
-        if (i >= 0 && j >= 0 && dp[i][j] != -1) {
-            return dp[i][j];
-        }
         if (i < 0) {
-            return findMinASCIISum1(string1, i, string2, j - 1, dp) + (int) string2.charAt(j);
+            return findMinASCIISum1(str1, i, str2, j - 1, dp) + (int) str2.charAt(j);
         }
         if (j < 0) {
-            return findMinASCIISum1(string1, i - 1, string2, j, dp) + (int) string1.charAt(i);
+            return findMinASCIISum1(str1, i - 1, str2, j, dp) + (int) str1.charAt(i);
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (str1.charAt(i) == str2.charAt(j)) {
+            return findMinASCIISum1(str1, i - 1, str2, j - 1, dp);
         }
 
-        if (string1.charAt(i) == string2.charAt(j)) {
-            return findMinASCIISum1(string1, i - 1, string2, j - 1, dp);
-        }
-        int result1 = findMinASCIISum1(string1, i - 1, string2, j, dp) + (int) string1.charAt(i);
-        int result2 = findMinASCIISum1(string1, i, string2, j - 1, dp) + (int) string2.charAt(j);
+        int result1 = findMinASCIISum1(str1, i - 1, str2, j, dp) + (int) str1.charAt(i);
+        int result2 = findMinASCIISum1(str1, i, str2, j - 1, dp) + (int) str2.charAt(j);
         dp[i][j] = Math.min(result1, result2);
         return dp[i][j];
     }
@@ -61,10 +61,10 @@ public class ASCIISum {
         }
         for (int i = 1; i <= string1.length(); i++) {
             for (int j = 1; j <= string2.length(); j++) {
-                if (string1.charAt(i-1) == string2.charAt(j-1)) {
-                    dp[i][j] = dp[i -1][j -1];
+                if (string1.charAt(i - 1) == string2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.min(dp[i -1][j] + string1.codePointAt(i-1), dp[i][j - 1] + string2.codePointAt(j-1));
+                    dp[i][j] = Math.min(dp[i - 1][j] + string1.codePointAt(i - 1), dp[i][j - 1] + string2.codePointAt(j - 1));
                 }
             }
         }

da0f6c9b378dd937f67f6afc83436b15e497217f
rishabh <rishabh.kashyap@oracle.com>
2022-04-25 17:30:53 +0530

    Leetcode 494: Target sum

--- hackerrank/src/com/problem/solutions/leetcode/TargetSum494.java
+++ hackerrank/src/com/problem/solutions/leetcode/TargetSum494.java
@@ -3,13 +3,11 @@ package com.problem.solutions.leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 //Problem: https://leetcode.com/problems/target-sum/description/
 
-public class TargetSum {
+public class TargetSum494 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -21,6 +19,7 @@ public class TargetSum {
         int s = Integer.parseInt(bufferedReader.readLine());
         List<Character> list = new ArrayList<>();
         System.out.println(countWays(arr, s));
+        System.out.println(findWays(arr, s));
     }
 
     private static int countWays(int[] arr, int s) {
@@ -69,5 +68,27 @@ public class TargetSum {
         int positiveWays = countWaysRecursive(arr, target, sum + arr[index], index + 1);
         int negativeWays = countWaysRecursive(arr, target, sum - arr[index], index + 1);
         return positiveWays + negativeWays;
+    }
+
+    private static int findWays(int[] nums, int target) {
+        Map<String, Integer> map = new HashMap<>();
+        return findWays(nums, target, 0, 0, map);
+    }
+
+    private static int findWays(int[] nums, int target, int sum, int i, Map<String, Integer> map) {
+        if (target == sum && i == nums.length) {
+            return 1;
+        }
+        if (i >= nums.length) {
+            return 0;
+        }
+        String key = "" + sum + "_" + i;
+        if (map.containsKey(key)) {
+            return map.get(key);
+        }
+        int result1 = findWays(nums, target, sum + nums[i], i + 1, map);
+        int result2 = findWays(nums, target, sum - nums[i], i + 1, map);
+        map.put(key, result1 + result2);
+        return result1 + result2;
     }
 }

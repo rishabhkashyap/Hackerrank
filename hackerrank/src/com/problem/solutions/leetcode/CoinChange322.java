820cd8088085f08df449b532924992bde4324c7b
rishabhkashyap <kashyap.rishab@hotmail.com>
2022-05-27 07:01:24 +0530

    Updating comment

--- hackerrank/src/com/problem/solutions/leetcode/CoinChange322.java
+++ hackerrank/src/com/problem/solutions/leetcode/CoinChange322.java
@@ -40,13 +40,14 @@ public class CoinChange322 {
 	}
 
 	private static int minCoinsChange2(int[] coins, int amount) {
-		int[][] dp = new int[coins.length+1][amount + 1];
+		int[][] dp = new int[coins.length + 1][amount + 1];
+
+		//Initializing first 2 rows is enough to fill rest of table
 		for (int i = 0; i <= coins.length; ++i) {
 			for (int j = 1; j <= amount; ++j) {
 				dp[i][j] = 99999;
 			}
 		}
-		//Initializing first 2 rows is enough to fill rest of table
 		for (int i = 1; i <= coins.length; i++) {
 			for (int j = 1; j <= amount; j++) {
 				if(j < coins[i - 1]) {

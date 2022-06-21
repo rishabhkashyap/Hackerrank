a7ddd21a15f64d85a6fd228e0598ec8cc4bd4c1a
rishabh kashyap <kashyap.rishab@gmail.com>
2022-06-21 17:19:07 +0530

    Leetcode 152:  Maximum Product Subarray

--- hackerrank/src/com/problem/solutions/leetcode/MaxProduct152.java
+++ hackerrank/src/com/problem/solutions/leetcode/MaxProduct152.java
@@ -1,5 +1,9 @@
 package com.problem.solutions.leetcode;
 
+import java.util.Arrays;
+
+//Problem: https://leetcode.com/problems/maximum-product-subarray/
+
 public class MaxProduct152 {
     public static void main(String[] args) {
         int[] arr = {2, 3, -2, 4};
@@ -7,17 +11,20 @@ public class MaxProduct152 {
     }
 
     private static int findMaxProduct1(int[] arr) {
-        int maxProd = Integer.MIN_VALUE;
+        int maxProd = Arrays.stream(arr).max().getAsInt();
         int maxProductSoFar = 1;
-        int negativeProd=Integer.MAX_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            int product=
-            if (maxProd < maxProductSoFar) {
-                maxProd = maxProductSoFar;
-            }
-            if (maxProductSoFar < 0) {
-                negativeProd=1;
+        int minProductSoFar = 1;
+        for (int ele : arr) {
+            if (ele == 0) {
+                minProductSoFar = 1;
                 maxProductSoFar = 1;
+            } else {
+
+                int temp = maxProductSoFar;
+                maxProductSoFar = Math.max(Math.max(maxProductSoFar * ele, minProductSoFar * ele), ele);
+                //Since max product so far is changed, using value stored int temp
+                minProductSoFar = Math.min(Math.min(temp * ele, minProductSoFar * ele), ele);
+                maxProd = Math.max(maxProd, maxProductSoFar);
             }
 
         }

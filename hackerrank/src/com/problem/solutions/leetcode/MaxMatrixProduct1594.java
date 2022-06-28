cfde7d3e4dd8a9481046def63fefe87e7f7b4179
rishabh <rishabh.kashyap@oracle.com>
2022-06-28 17:49:38 +0530

    Leetcode 1594 Max non negative product

--- hackerrank/src/com/problem/solutions/leetcode/MaxMatrixProduct1594.java
+++ hackerrank/src/com/problem/solutions/leetcode/MaxMatrixProduct1594.java
@@ -2,25 +2,42 @@ package com.problem.solutions.leetcode;
 
 public class MaxMatrixProduct1594 {
     private static final int MODULO = 10 ^ 9 + 7;
+    private static long result = -1;
 
     public static void main(String[] args) {
-        //  int[][] grid = {{1, -2, 1}, {1, -2, 1}, {3, -4, 1}};
+        //int[][] grid = {{1, -2, 1}, {1, -2, 1}, {3, -4, 1}};
         int[][] grid = {{-1, -2, -3}, {-2, -3, -3}, {-3, -3, -2}};
-        System.out.println(maxProduct(grid));
+       // int[][] grid={{1,3},{0,-4}};
+        maxProduct(grid);
+        System.out.println(result < 0 ? -1 : (int)(result % MODULO));
     }
 
-    private static int maxProduct(int[][] grid) {
-        int result = maxProduct(grid, 0, 0);
-        return result >= 0 ? result : -1;
+    private static void maxProduct(int[][] grid) {
+        maxProduct(grid, 0, 0, grid[0][0]);
     }
 
-    private static int maxProduct(int[][] grid, int i, int j) {
+    private static void maxProduct(int[][] grid, int i, int j, long product) {
         if (i >= grid.length || j >= grid[0].length) {
-            return 1;
+            return;
         }
-        int result1 = maxProduct(grid, i + 1, j) * grid[i][j];
-        int result2 = maxProduct(grid, i, j + 1) * grid[i][j];
-        return Math.max(result1, result2) % MODULO;
+
+        if (i == grid.length - 1 && j == grid[0].length - 1) {
+            result = Math.max(result, product);
+            return;
+        }
+
+        if (grid[i][j] == 0) {
+            result = Math.max(result, 0);
+            // System.out.println("inside zero = "+result);
+            return;
+        }
+        if (i + 1 < grid.length) {
+            maxProduct(grid, i + 1, j, product * grid[i + 1][j]);
+        }
+        if (j + 1 < grid[0].length) {
+            maxProduct(grid, i, j + 1, product * grid[i][j + 1]);
+        }
+
 
     }
 }

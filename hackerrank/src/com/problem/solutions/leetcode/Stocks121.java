497653d51975a45ff783c98d296bf5d5cc1fdde0
rishabh <rishabh.kashyap@oracle.com>
2022-07-07 17:27:11 +0530

    Leetcode 121: Best Time to Buy and Sell Stock

---
+++ hackerrank/src/com/problem/solutions/leetcode/Stocks121.java
@@ -0,0 +1,21 @@
+package com.problem.solutions.leetcode;
+
+//Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+public class Stocks121 {
+    public static void main(String[] args) {
+        int[] arr = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(arr));
+
+    }
+
+    public static int maxProfit(int[] prices) {
+        int lowestPrice = Integer.MAX_VALUE;
+        int profit = Integer.MIN_VALUE;
+
+        for (int price : prices) {
+            profit = Math.max(profit, price - lowestPrice);
+            lowestPrice = Math.min(lowestPrice, price);
+        }
+        return profit < 0 ? 0 : profit;
+    }
+}

d2f47e86081835046bbb85edb98a7d4505d94ae0
rishabh kashyap <kashyap.rishab@hotmail.com>
2022-07-08 18:29:38 +0530

    Leetcode 122: Best Time to Buy and Sell Stock II

--- hackerrank/src/com/problem/solutions/leetcode/Stock122.java
+++ hackerrank/src/com/problem/solutions/leetcode/Stock122.java
@@ -1,33 +1,60 @@
 package com.problem.solutions.leetcode;
 
+import java.util.Arrays;
+//Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 public class Stock122 {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-        System.out.println(maxProfit(arr));
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(maxStockProfit(arr));
+        System.out.println(maxStockProfit2(arr));
     }
 
-    private static int maxProfit(int[] prices) {
-        boolean canSell = false;
-        return maxProfit(prices, canSell, 0);
+    private static int maxStockProfit(int[] prices) {
+        int[][] dp = new int[prices.length + 1][2];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+        return maxStockProfit(prices, 0, 0, dp);
     }
 
-    private static int maxProfit(int[] prices, boolean canSell, int i) {
+    private static int maxStockProfit(int[] prices, int canSell, int i, int[][] dp) {
         if (i >= prices.length) {
             return 0;
+        }
+        if (dp[i][canSell] != -1) {
+            return dp[i][canSell];
         }
         int op1 = 0;
         int op2 = 0;
         int op3 = 0;
-        int op4=0;
-        if (!canSell) {
-            op1 = maxProfit(prices, true, i + 1) - prices[i];
-            op2 = maxProfit(prices, false, i + 1);
+        int op4 = 0;
+        if (canSell == 0) {
+            op1 = maxStockProfit(prices, 1, i + 1, dp) - prices[i];
+            op2 = maxStockProfit(prices, 0, i + 1, dp);
         }
-        if (canSell) {
-            op3 = maxProfit(prices, false, i + 1) + prices[i];
-            op4 = maxProfit(prices, true, i + 1);
+        if (canSell == 1) {
+            op3 = maxStockProfit(prices, 0, i + 1, dp) + prices[i];
+            op4 = maxStockProfit(prices, 1, i + 1, dp);
         }
-        return Math.max(Math.max(op1, op2), Math.max(op3,op4));
+        dp[i][canSell] = Math.max(Math.max(op1, op2), Math.max(op3, op4));
+        return dp[i][canSell];
 
+    }
+
+    private static int maxStockProfit2(int[] prices) {
+        int buy = prices[0];
+        int sell = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (sell < prices[i]) {
+                sell = prices[i];
+            } else {
+                profit += (sell - buy);
+                buy = prices[i];
+                sell = prices[i];
+            }
+        }
+        profit += (sell - buy);
+        return profit;
     }
 }

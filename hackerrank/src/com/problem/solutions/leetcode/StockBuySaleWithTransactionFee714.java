b3abcac56283f1d8636ff4e01f36ca5fe0e27c97
rishabh kashyap <kashyap.rishab@hotmail.com>
2022-07-27 17:56:52 +0530

    Leetcode 714: Buy and sell to make maximum profit with transaction fee

--- hackerrank/src/com/problem/solutions/leetcode/StockBuySaleWithTransactionFee714.java
+++ hackerrank/src/com/problem/solutions/leetcode/StockBuySaleWithTransactionFee714.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class StockBuySaleWithTransactionFee {
+public class StockBuySaleWithTransactionFee714 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));

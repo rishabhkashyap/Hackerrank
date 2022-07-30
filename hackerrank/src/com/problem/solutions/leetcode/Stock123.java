28feefce173fb8c55f0842ce9d9c6497c90cfa81
rishabh kashyap <kashyap.rishab@hotmail.com>
2022-07-30 07:51:00 +0530

    Code refactoring

--- hackerrank/src/com/problem/solutions/leetcode/Stock123.java
+++ hackerrank/src/com/problem/solutions/leetcode/Stock123.java
@@ -6,11 +6,11 @@ public class Stock123 {
     public static void main(String[] args) {
         int[] prices = {1, 2, 3, 4, 5};
         int k = 2;
-        System.out.println(maxProfit1(prices, k));
-        System.out.println(maxProfit2(prices, k));
+        System.out.println(maxProfit1(prices));
+        System.out.println(maxProfit2(prices));
     }
 
-    private static int maxProfit1(int[] prices, int k) {
+    private static int maxProfit1(int[] prices) {
         int[][][] dp = new int[prices.length][2][3];
         for (int i = 0; i < dp.length; i++) {
             for (int j = 0; j < 2; j++) {
@@ -19,7 +19,7 @@ public class Stock123 {
                 }
             }
         }
-        return maxProfit1(prices, 0, 0, k, dp);
+        return maxProfit1(prices, 0, 0, 2, dp);
     }
 
     private static int maxProfit1(int[] prices, int i, int canSell, int k, int[][][] dp) {
@@ -31,53 +31,37 @@ public class Stock123 {
         }
         int op1 = 0;
         int op2 = 0;
-        int op3 = 0;
-        int op4 = 0;
         if (canSell == 0) {
             op1 = maxProfit1(prices, i + 1, 1, k, dp) - prices[i];
             op2 = maxProfit1(prices, i + 1, 0, k, dp);
         }
         if (canSell == 1) {
-            op3 = maxProfit1(prices, i + 1, 0, k - 1, dp) + prices[i];
-            op4 = maxProfit1(prices, i + 1, 1, k, dp);
+            op1 = maxProfit1(prices, i + 1, 0, k - 1, dp) + prices[i];
+            op2 = maxProfit1(prices, i + 1, 1, k, dp);
         }
-        dp[i][canSell][k] = Math.max(Math.max(op1, op2), Math.max(op3, op4));
+        dp[i][canSell][k] = Math.max(op1, op2);
         return dp[i][canSell][k];
     }
 
-    private static int maxProfit2(int[] prices, int k) {
-        int buy = prices[0];
+    private static int maxProfit2(int[] prices) {
         //Stores max profit if stock is sold before ith day or on ith day
         int[] profitTillDate = new int[prices.length];
         //Stores max profit when stock is bought on i th day and sold on any day after i th day
         int[] profitAfterDate = new int[prices.length];
-        int profit = 0;
+        int buy = prices[0];
         for (int i = 1; i < prices.length; i++) {
-            profit = Math.max(profit, prices[i] - buy);
-            if (profit > profitTillDate[i - 1]) {
-                profitTillDate[i] = profit;
-            } else {
-                profitTillDate[i] = profitTillDate[i - 1];
-            }
+            profitTillDate[i] = Math.max(profitTillDate[i - 1], prices[i] - buy);
             buy = Math.min(buy, prices[i]);
         }
         int sell = prices[prices.length - 1];
-        profit = Integer.MIN_VALUE;
         for (int i = prices.length - 2; i >= 0; i--) {
-            profit = Math.max(profit, sell - prices[i]);
-            if (profit > profitAfterDate[i + 1]) {
-                profitAfterDate[i] = profit;
-            } else {
-                profitAfterDate[i] = profitAfterDate[i + 1];
-            }
+            profitAfterDate[i] = Math.max(profitAfterDate[i + 1], sell - prices[i]);
             sell = Math.max(sell, prices[i]);
         }
-        profit = 0;
+        int profit = 0;
         for (int i = 0; i < prices.length; i++) {
             profit = Math.max(profit, profitTillDate[i] + profitAfterDate[i]);
         }
         return profit;
-
-
     }
 }
